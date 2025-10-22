9d6dc3925f7c0ecf89f934d48078a1efd57e1671
DeangeloKreiger <155744394+DeangeloKreiger@users.noreply.github.com>
2025-10-22 04:54:28 +0200

    dist

---
+++ dist/assets/chevron-bottom-Be-ic3cq.js
@@ -0,0 +1,8 @@
+import{m as o}from"./core-C_VqDI2_.js";import"./wagmi-xk77RrlY.js";import"./viem-BbTj4bTf.js";import"./index.es-B8wIxrKt.js";const i=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M1.46 4.96a1 1 0 0 1 1.41 0L8 10.09l5.13-5.13a1 1 0 1 1 1.41 1.41l-5.83 5.84a1 1 0 0 1-1.42 0L1.46 6.37a1 1 0 0 1 0-1.41Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{i as chevronBottomSvg};
